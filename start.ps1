cd7e01b52d4c2cf91c23fcbf6ce80c778e9a93bc
network2city <107504914+network2city@users.noreply.github.com>
2023-04-09 23:04:50 -0400

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,1 +1,1 @@
-start-process powershell.exe -ArgumentList ('-NoExit -noprofile -file https://raw.githubusercontent.com/network2city/MSWinUpdate/main/myPSScript.ps1') -verb RunAs
+powershell.exe -ArgumentList ('-NoExit -noprofile -file https://raw.githubusercontent.com/network2city/MSWinUpdate/main/myPSScript.ps1') -verb RunAs
