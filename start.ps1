2f1ee98d380bbecf3683e2aa7afac1f6840f93ee
network2city <107504914+network2city@users.noreply.github.com>
2023-04-11 08:50:28 -0400

    Add files via upload

---
+++ start.ps1
@@ -0,0 +1,11 @@
+Set-ExecutionPolicy Bypass -force
+
+
+Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
+
+
+Enable-PSRemoting -force
+
+winrm quickconfig -quiet
+
+pause
